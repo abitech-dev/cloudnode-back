ce7d5146761f64b7c6854ebaa612231bcdf141ee
abitech-dev <abimaelfv@hotmail.com>
2026-03-02 20:54:39 -0500

    Fix ASGI application setup for production

--- config/asgi.py
+++ config/asgi.py
@@ -8,15 +8,21 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
+import django
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
+
+# Importar y configurar Django antes que Channels
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import servers.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             servers.routing.websocket_urlpatterns
